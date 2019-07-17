72378cfb989305be35462c1e4d501da59c6c74af
satish070482 <satishkhobragade@gmail.com>
2019-07-17 10:08:47 +0000

    adding script

---
+++ script.sh
@@ -0,0 +1,8 @@
+echo "#####################################"
+echo "######STARTING SCRIPT EXECUTION#####"
+echo "#####################################"
+yum -y install talk
+useradd yogeshraheja
+echo "#####################################"
+echo "#########ENDING SCRIPT EXECUTION#####"
+echo "#####################################"
